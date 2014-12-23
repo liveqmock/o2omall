afa131374b9aebc2f0768792ab3380998714f392
zhong0711101 <zhong-lj@163.com>
2014-12-23 10:43:58 +0800

    add mall

---
+++ code/awe-mall/awe-mall-web/src/test/java/com/awe/test/base/BaseTransactionTestCase.java
@@ -0,0 +1,26 @@
+package com.awe.test.base;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+/**
+ * Base TestCase with Transaction
+ * 
+ * @author ljz
+ * @version 2014-12-23 10:38:53
+ * 
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { TestConstants.LOCATIONS })
+@TransactionConfiguration(transactionManager = TestConstants.TRANSACTION_MANAGER, defaultRollback = true)
+@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class,
+        TransactionalTestExecutionListener.class })
+public abstract class BaseTransactionTestCase extends AbstractTransactionalJUnit4SpringContextTests {
+
+}
