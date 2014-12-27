79f8b774783d410fe74c7548abde4d3eee0d52cb
jss89757 <jss89757@163.com>
2014-12-27 17:16:01 +0800

    调用area接口

---
+++ code/awe-order/awe-order-service/src/main/java/com/awe/order/service/AreaService.java
@@ -0,0 +1,42 @@
+package com.awe.order.service;
+
+import java.util.List;
+
+import com.awe.uc.sdk.response.dto.AreaResponseDto;
+
+
+/**
+ * 三级地址服务
+ * 
+ * @author ljz
+ * @version 2014-12-23 10:06:48
+ * 
+ */
+public interface AreaService {
+
+	/**
+	 * 获取所有省
+	 * 
+	 * @return
+	 */
+	List<AreaResponseDto> getProvinces();
+
+	/**
+	 * 获取省下面的所有市
+	 * 
+	 * @param provinceCode
+	 *            省编码
+	 * @return
+	 */
+	List<AreaResponseDto> getCities(String provinceCode);
+
+	/**
+	 * 获取市下面的所有县
+	 * 
+	 * @param cityCode
+	 *            市编码
+	 * @return
+	 */
+	List<AreaResponseDto> getCountys(String cityCode);
+
+}
