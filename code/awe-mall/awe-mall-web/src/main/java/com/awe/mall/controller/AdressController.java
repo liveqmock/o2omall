0b0783752547770e9dc9fd78127d9119374da6ca
zhangyqpost@126.com <zhangyqpost@126.com>
2014-12-30 10:06:22 +0800

    挂基础资料和收货地址页面

--- code/awe-mall/awe-mall-web/src/main/java/com/awe/mall/controller/AdressController.java
+++ code/awe-mall/awe-mall-web/src/main/java/com/awe/mall/controller/AdressController.java
@@ -5,25 +5,25 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
- * @description 个人基本信息
+ * @description 送货地址
  * @author zyq
  * @date 2014-12-30
  * @version 1.0.0
  */
 @Controller
-@RequestMapping("profile")
-public class ProfileController {
+@RequestMapping("address")
+public class AdressController {
 
-	private static final Log LOG = LogFactory.getLog(ProfileController.class);
+	private static final Log LOG = LogFactory.getLog(AdressController.class);
 	
 	private static final String VIEW_WORKSPACE = "myorder/";
-	private static final String VIEW_PAGE = " personalProfile";
-//	private static final String VIEW_ADD_SUCCESS = "addCartSuccess";
+	private static final String VIEW_PAGE = " deliveryAddress";
 	
-	@RequestMapping("myprofile")
-	public String profile(Model model){
-		LOG.info("-- welcome to myProfile page --");
+	@RequestMapping("list")
+	public String list(Model model){
+		LOG.info("-- welcome to list page --");
 		return VIEW_WORKSPACE + VIEW_PAGE;
 	}
 	
