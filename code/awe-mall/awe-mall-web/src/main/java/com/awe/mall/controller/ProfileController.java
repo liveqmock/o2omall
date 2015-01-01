18bfd6b4eca21bb0a2d41a95253bc18e034cf9a0
zhangyqpost@126.com <zhangyqpost@126.com>
2015-01-01 11:15:27 +0800

    【后台代码】个人基本资料添加、编辑功能

--- code/awe-mall/awe-mall-web/src/main/java/com/awe/mall/controller/ProfileController.java
+++ code/awe-mall/awe-mall-web/src/main/java/com/awe/mall/controller/ProfileController.java
@@ -5,6 +5,9 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.awe.uc.sdk.request.dto.UserProfileRequestDto;
 /**
  * @description 个人基本信息
  * @author zyq
@@ -20,16 +23,33 @@ public class ProfileController {
 	private static final String VIEW_WORKSPACE = "myorder/";
 	private static final String VIEW_PAGE = " personalProfile";
 //	private static final String VIEW_ADD_SUCCESS = "addCartSuccess";
-	
+	/**
+	 * 打开个人基础资料首页面
+	 */
 	@RequestMapping("myprofile")
 	public String profile(Model model){
 		LOG.info("-- welcome to myProfile page --");
 		return VIEW_WORKSPACE + VIEW_PAGE;
 	}
-	
+	/**
+	 * 编辑个人基本资料
+	 * @param model
+	 * @param profile
+	 * @return
+	 */
 	@RequestMapping("edit")
-	public String edit(Model model){
+	public String edit(Model model,UserProfileRequestDto profile){
 		LOG.info("-- welcome to edit page --");
 		return VIEW_WORKSPACE + VIEW_PAGE;
+	}
+	/**
+	 * 完善个人基础资料
+	 * @param model
+	 * @param profile
+	 * @return
+	 */
+	@RequestMapping(value = "add", method = RequestMethod.POST)
+	public String add(Model model,UserProfileRequestDto profile){
+		return "";
 	}
 }
