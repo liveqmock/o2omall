b9fb696c7c5a59fd3d2866d434fd3a941906e794
zhc2054 <zhc2054@163.com>
2015-01-02 22:06:16 +0800

    商品类别、品牌、商家客户端

--- code/awe-pms-sdk/src/main/java/com/awe/pms/sdk/ProductCategoryClient.java
+++ code/awe-pms-sdk/src/main/java/com/awe/pms/sdk/ProductCategoryClient.java
@@ -1,15 +1,18 @@
 package com.awe.pms.sdk;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.util.Assert;
 
-import com.hbird.common.client.AbstractSecureClient;
-import com.hbird.common.utils.serialize.JsonHelper;
 import com.awe.pms.sdk.request.ProductCategoryRequest;
 import com.awe.pms.sdk.request.dto.ProductCategoryRequestDto;
 import com.awe.pms.sdk.response.ProductCategoryResponse;
 import com.awe.pms.sdk.response.dto.ProductCategoryResponseDto;
+import com.hbird.common.client.AbstractSecureClient;
+import com.hbird.common.sdk.api.response.HbirdResponse;
+import com.hbird.common.utils.serialize.JsonHelper;
 
 /**
  * 商品类别服务的客户端
@@ -46,5 +49,36 @@ public class ProductCategoryClient extends AbstractSecureClient {
             LOG.debug("getProductCategory response: " + JsonHelper.toJson(response));
         }
         return super.getResult(response);
+    }
+    
+    /**
+     * 根据条件查询商品类别服务
+     * 
+     * @param request
+     *            查询请求对象
+     * @return List<ProductCategoryResponseDto> 接口返回的数据对象集合
+     */
+    @SuppressWarnings("unchecked")
+	public List<ProductCategoryResponseDto> getProductCategorys(ProductCategoryRequestDto requestDto) {
+    	Assert.notNull(requestDto);
+    	
+    	ProductCategoryRequest request = new ProductCategoryRequest(super.getKey(), requestDto);
+    	
+    	if (LOG.isDebugEnabled()) {
+    		LOG.debug("getProductCategorys request: " + JsonHelper.toJson(request));
+    	}
+    	
+    	String url = super.getServiceUrlDomain() + "services/productCategory/getProductCategorys";
+    	
+    	HbirdResponse<List> response = super.getRestTemplate().postForObject(url, request, HbirdResponse.class);
+    	
+    	List<ProductCategoryResponseDto> responseResult = JsonHelper.toList(JsonHelper.toJson(response.getResult()), ProductCategoryResponseDto.class);
+    	response.setResult(responseResult);
+    	
+    	if (LOG.isDebugEnabled()) {
+    		LOG.debug("getProductCategorys url: " + url);
+    		LOG.debug("getProductCategorys response: " + JsonHelper.toJson(response));
+    	}
+    	return super.getResult(response);
     }
 }
