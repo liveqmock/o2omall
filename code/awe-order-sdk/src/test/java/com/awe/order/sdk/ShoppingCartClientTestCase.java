afe21961a826f0cbcc518b97d149bfd96eb3f67c
zhangyqpost <zhangyqpost@126.com>
2015-01-05 18:44:05 +0800

    单元测试fixbug

--- code/awe-order-sdk/src/test/java/com/awe/order/sdk/ShoppingCartClientTestCase.java
+++ code/awe-order-sdk/src/test/java/com/awe/order/sdk/ShoppingCartClientTestCase.java
@@ -1,11 +1,15 @@
 package com.awe.order.sdk;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
 import com.awe.order.sdk.request.dto.ShoppingCartRequestDto;
 import com.awe.order.sdk.response.dto.ShoppingCartResponseDto;
+import com.hbird.common.utils.wrap.WrapMapper;
+import com.hbird.common.utils.wrap.Wrapper;
 
 /**
  * ShoppingCartClient测试用例
@@ -15,8 +19,8 @@ import com.awe.order.sdk.response.dto.ShoppingCartResponseDto;
  * 
  */
 public class ShoppingCartClientTestCase {
-    String WS_DOMAIN = "http://dev.orderws.shop.hbird.com/";
-    // String WS_DOMAIN = "http://local.orderws.shop.hbird.com:8090/";
+	//String WS_DOMAIN = "http://dev.orderws.shop.hbird.com/";
+     String WS_DOMAIN = "http://local.orderws.shop.hbird.com:8080/";
     private ShoppingCartClient client;
 
     @Before
@@ -28,7 +32,9 @@ public class ShoppingCartClientTestCase {
         client.setReadTimeout(3000);
         client.afterPropertiesSet();
     }
-
+    /**
+     * 购物车列表数据获取
+     */
     @Test
     public void testGetShoppingCart() {
         ShoppingCartRequestDto requestDto = new ShoppingCartRequestDto();
@@ -37,5 +43,42 @@ public class ShoppingCartClientTestCase {
         ShoppingCartResponseDto shoppingCartResponseDto = client.getShoppingCart(requestDto);
         Assert.notNull(shoppingCartResponseDto);
     } 
-
+    /**
+     * 删除购物车指定商品数据
+     */
+    @Test
+    public void deleteShoppingCartById(){
+    	ShoppingCartRequestDto requestDto = new ShoppingCartRequestDto();
+        requestDto.setId(1l);
+        
+        Wrapper<?> wrapper = client.deleteShoppingCartById(requestDto);
+        Assert.isTrue(WrapMapper.ok().getMessage().equals(wrapper.getMessage()));
+    }
+    /**
+     * 添加购物车
+     */
+    @Test
+    public void addShoppingCart(){
+    	ShoppingCartRequestDto requestDto = new ShoppingCartRequestDto();
+        requestDto.setSeller("好再来商家");
+        requestDto.setSellerNo("120");
+        requestDto.setSkuCount(1);
+        requestDto.setSkuNo("1245214");
+        requestDto.setStatus(1);
+        requestDto.setUserNo("5");
+        requestDto.setCreateUser("张三");
+        requestDto.setUpdateUser("张三");
+        Wrapper<?> wrapper = client.addShoppingCart(requestDto);
+        Assert.isTrue(WrapMapper.ok().getMessage().equals(wrapper.getMessage()));
+    }
+    /**
+     * 购物车列表数据获取
+     */
+    @Test
+    public void queryShoppingCartList(){
+    	ShoppingCartRequestDto requestDto = new ShoppingCartRequestDto();
+    	requestDto.setUserNo("1");
+    	List<ShoppingCartResponseDto> responseDtoList = client.queryShoppingCartList(requestDto);
+    	Assert.notNull(responseDtoList);
+    }
 }
