116779d12866da663926306be45bc30ebda00869
zhc2054 <zhc2054@163.com>
2015-01-17 21:22:28 +0800

    商品信息字典表

--- code/awe-pms-sdk/src/main/java/com/awe/pms/sdk/ProductDictClient.java
+++ code/awe-pms-sdk/src/main/java/com/awe/pms/sdk/ProductDictClient.java
@@ -1,15 +1,18 @@
 package com.awe.pms.sdk;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.util.Assert;
 
-import com.hbird.common.client.AbstractSecureClient;
-import com.hbird.common.utils.serialize.JsonHelper;
 import com.awe.pms.sdk.request.ProductDictRequest;
 import com.awe.pms.sdk.request.dto.ProductDictRequestDto;
 import com.awe.pms.sdk.response.ProductDictResponse;
 import com.awe.pms.sdk.response.dto.ProductDictResponseDto;
+import com.hbird.common.client.AbstractSecureClient;
+import com.hbird.common.sdk.api.response.HbirdResponse;
+import com.hbird.common.utils.serialize.JsonHelper;
 
 /**
  * 配置表服务的客户端
@@ -46,5 +49,35 @@ public class ProductDictClient extends AbstractSecureClient {
             LOG.debug("getProductDict response: " + JsonHelper.toJson(response));
         }
         return super.getResult(response);
+    }
+    
+    /**
+     * 配置表查询服务-查询所有数据
+     * 
+     * @param request
+     *            查询请求对象
+     * @return List<ProductDictResponseDto> 接口返回的数据对象
+     */
+    @SuppressWarnings("unchecked")
+	public List<ProductDictResponseDto> getAllProductDict(ProductDictRequestDto requestDto) {
+//    	Assert.notNull(requestDto);
+    	
+    	ProductDictRequest request = new ProductDictRequest(super.getKey(), requestDto);
+    	
+    	if (LOG.isDebugEnabled()) {
+    		LOG.debug("getAllProductDict request: " + JsonHelper.toJson(request));
+    	}
+    	
+    	String url = super.getServiceUrlDomain() + "services/productDict/getAllProductDict";
+    	HbirdResponse<List> response = super.getRestTemplate().postForObject(url, request, HbirdResponse.class);
+    	
+    	List<ProductDictResponseDto> responseResult = JsonHelper.toList(JsonHelper.toJson(response.getResult()), ProductDictResponseDto.class);
+    	response.setResult(responseResult);
+    	
+    	if (LOG.isDebugEnabled()) {
+    		LOG.debug("getAllProductDict url: " + url);
+    		LOG.debug("getAllProductDict response: " + JsonHelper.toJson(response));
+    	}
+    	return super.getResult(response);
     }
 }
--- code/awe-pms-sdk/src/test/java/com/awe/pms/sdk/ProductDictClientTestCase.java
+++ code/awe-pms-sdk/src/test/java/com/awe/pms/sdk/ProductDictClientTestCase.java
@@ -1,5 +1,7 @@
 package com.awe.pms.sdk;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.util.Assert;
@@ -15,8 +17,8 @@ import com.awe.pms.sdk.response.dto.ProductDictResponseDto;
  * 
  */
 public class ProductDictClientTestCase {
-    String WS_DOMAIN = "http://dev.pmsws.shop.hbird.com/";
-    // String WS_DOMAIN = "http://local.pmsws.shop.hbird.com:8090/";
+//    String WS_DOMAIN = "http://dev.pmsws.shop.hbird.com/";
+    String WS_DOMAIN = "http://local.pmsws.shop.hbird.com:8100/";
     private ProductDictClient client;
 
     @Before
@@ -36,6 +38,15 @@ public class ProductDictClientTestCase {
         
         ProductDictResponseDto productDictResponseDto = client.getProductDict(requestDto);
         Assert.notNull(productDictResponseDto);
+    } 
+    
+    @Test
+    public void testGetAllProductDict() {
+    	ProductDictRequestDto requestDto = new ProductDictRequestDto();
+//    	requestDto.setId(1l);
+    	
+    	List<ProductDictResponseDto> productDictResponseDtos = client.getAllProductDict(requestDto);
+    	Assert.notNull(productDictResponseDtos);
     } 
 
 }
