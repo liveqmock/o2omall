50e4f532e171b8b383d9cba03b68fb147e409abe
zhangyqpost@126.com <zhangyqpost@126.com>
2015-01-21 23:43:22 +0800

    rems-sdk 枚举

---
+++ code/awe-rems-sdk/src/main/java/com/awe/rems/enums/EnumDataEntity.java
@@ -0,0 +1,30 @@
+package com.awe.rems.enums;
+/**
+ * @description 下拉列表所用实体
+ * @author zyq
+ * @since 2014-11-11
+ */
+public class EnumDataEntity {
+	/**键*/
+	private int key;
+	/**值*/
+	private String value;
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
